9587ff9fe6f31337507c4fe06d505cdc953eb746
David Predusel <theprdv@yahoo.com>
2022-10-05 14:39:52 +0300

    Update Withdrawable.sol

--- contracts/utils/Withdrawable.sol
+++ contracts/utils/Withdrawable.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.6;
+pragma solidity ^0.8.17;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/utils/SafeERC20.sol";
